fcf4fa7339e7ad3014d3a82cd32a8b7c8fac7966
XPromus <hilgi45@gmail.com>
2024-03-14 18:51:52 +0100

    Initial commit.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { createScene } from "../graphics/defaultScene";
+    import { onMount } from "svelte";
+
+    let el: HTMLCanvasElement;
+
+    onMount(() => {
+        createScene(el);
+    })
+
+</script>
+
+<div class="w-screen h-screen">
+    <canvas class="z-0" bind:this={el} />
+    <div class="w-full h-full z-50 text-white top-0 left-0 absolute">
+        Hello
+    </div>
+</div>
