278ab76c447b45215c7774337b5250cc13e3ade9
Christopher Hilgner <hilgi45@gmail.com>
2024-05-29 09:25:45 +0200

    Access to house children.

--- src/lib/graphics/fbxLoader.ts
+++ src/lib/graphics/fbxLoader.ts
@@ -1,3 +1,5 @@
+import { houseChildrenNameStore } from "$lib/data/houseStore";
+import { get } from "svelte/store";
 import type { Scene } from "three";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 
@@ -10,6 +12,14 @@ export const loadFbxHouse = (scene: Scene) => {
             console.log("Objekt geladen.");
             object.scale.set(1, 1, 1);
             object.position.set(0, 0, -5);
+
+            for (let i = 0; i < object.children.length; i++) {
+                const childName: string = object.children[i].name;
+                const newArray: string[] = get(houseChildrenNameStore);
+                newArray.push(childName);
+                houseChildrenNameStore.set(newArray);
+            }
+
             scene.add(object);
         },
         (xhr) => {
