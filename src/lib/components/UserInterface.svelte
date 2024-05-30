d8bed0f89287496e84bc6fa572d22d0ff8ca21b1
XPromus <hilgi45@gmail.com>
2024-05-30 13:20:17 +0200

    Refactor and list for house components can be toggled.

--- src/lib/components/UserInterface.svelte
+++ src/lib/components/UserInterface.svelte
@@ -11,11 +11,11 @@
     }
 </script>
 
-<div class="w-full h-full flex flex-row overflow-x-scroll bg-white p-2 space-x-2">
+<div class="flex overflow-x-scroll flex-row p-2 space-x-2 w-full h-full bg-white">
     <!--
-    <div class="w-full flex flex-row space-x-2 p-2">
+    <div class="flex flex-row p-2 space-x-2 w-full">
         <input bind:value={inputValue} class="p-2 rounded-md grow" type="text" placeholder="FBX Model Path">
-        <button on:click={loadModelButtonClicked} class="bg-green-400 py-2 px-6 rounded-md">
+        <button on:click={loadModelButtonClicked} class="py-2 px-6 bg-green-400 rounded-md">
             Load
         </button>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,31 @@
 
     let el: HTMLCanvasElement;
 
+    let showParts: boolean = false;
+
     onMount(() => {
         createScene(el);
     });
 </script>
 
-<canvas class="z-0 absolute top-0 left-0" bind:this={el} />
+<canvas class="absolute top-0 left-0 z-0" bind:this={el} />
 
-<div class="w-screen h-screen overflow-y-hidden">
-    <div class="w-full h-fit z-50 top-0 left-0 absolute">
-        <UserInterface />
+<div class="overflow-y-hidden w-screen h-screen">
+    <div class="flex absolute top-0 left-0 z-50 flex-col p-1 space-y-1 w-full h-fit">
+        <div class="flex flex-row space-x-2 w-full h-fit">
+            {#if showParts}
+                <button on:click={() => {showParts = !showParts}} class="p-1 bg-white border border-black shadow-inner shadow-black">
+                    Show Parts
+                </button>
+            {:else}
+                <button on:click={() => {showParts = !showParts}} class="p-1 bg-white border border-black">
+                    Show Parts
+                </button>
+            {/if}
+            
+        </div>
+        {#if showParts}
+            <UserInterface />
+        {/if}
     </div>
 </div>
